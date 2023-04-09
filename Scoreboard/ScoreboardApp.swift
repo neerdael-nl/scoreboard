e2982fb5d86284be4484917fb81ef69fa3edd5f2
johnneerdael <john@neerdael.nl>
2023-04-09 16:42:53 +0200

    Initial Commit

---
+++ Scoreboard/ScoreboardApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScoreboardApp.swift
+//  Scoreboard
+//
+//  Created by John Neerdael on 09/04/2023.
+//
+
+import SwiftUI
+
+@main
+struct ScoreboardApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
